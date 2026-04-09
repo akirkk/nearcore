b0b09021632196a516ed8a04c3965bd3e7b2a06e
Adam Wierzbicki <adam.wierzbicki@nearone.org>
2026-04-09 12:03:17 +0200

    Stabilize dynamic resharding at protocol version 85

--- core/primitives-core/src/version.rs
+++ core/primitives-core/src/version.rs
@@ -489,7 +489,8 @@ impl ProtocolFeature {
             | ProtocolFeature::InstantDeleteAccount => 83,
             ProtocolFeature::Wasmtime => 84,
             ProtocolFeature::FixDelegateActionDepositWithFunctionCallError
-            | ProtocolFeature::ContinuousEpochSync => 85,
+            | ProtocolFeature::ContinuousEpochSync
+            | ProtocolFeature::DynamicResharding => 85,
 
             // Nightly features:
             ProtocolFeature::FixContractLoadingCost => 129,
@@ -497,7 +498,6 @@ impl ProtocolFeature {
             // that always enables this for mocknet (see config_mocknet function).
             ProtocolFeature::ShuffleShardAssignments => 143,
             ProtocolFeature::GasKeys => 149,
-            ProtocolFeature::DynamicResharding => 150,
             ProtocolFeature::StrictNonce => 151,
             ProtocolFeature::EarlyKickout => 152,
 
